66a6ce1788943e10533ca77f831ac2d1b513850e
mikx049 <mikolajrokita174@gmail.com>
2026-08-13 19:59:34 +0200

    logs

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,16 @@
 import sererConfig from "../.config/server.yml";
 
+const log = (data: any[]) => {
+  if (sererConfig.logs_enabled) {
+    
+  }
+}
+
 const server = Bun.serve({
   port: sererConfig.port,
 
   fetch(req) {
+    console.log("HTTP 200 OpenHttp ", req.url);
     return new Response("OpenHttp Server OK");
   },
 });
